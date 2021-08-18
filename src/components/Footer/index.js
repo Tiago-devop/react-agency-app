419844b56c6b0b8f5f5c2fd10f85fa807624bdc2
Tiagodev <tiagodev@protonmail.com>
2021-08-18 12:37:19 -0700

    add Foot component and styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,17 @@
-import { Container } from "./Footer.styles";
+import { Container, Copyright, List, ListItem, Wrapper } from "./Footer.styles";
 
 const Footer = () => (
   <Container>
-    Footer
-  </Container>  
-)
+    <Wrapper>
+      <List>
+        <ListItem>Guide</ListItem>
+        <ListItem>Support</ListItem>
+        <ListItem>API</ListItem>
+        <ListItem>Community</ListItem>
+      </List>
+      <Copyright>Tiago Dev &copy;</Copyright>
+    </Wrapper>
+  </Container>
+);
 
 export default Footer;
