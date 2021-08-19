72464518bc4b614588eff625ef63a1b717a3b03d
Tiagodev <tiagodev@protonmail.com>
2021-08-18 19:26:16 -0700

    add responsivity

--- src/components/Contact/Contact.styles.js
+++ src/components/Contact/Contact.styles.js
@@ -11,15 +11,27 @@ export const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `;
 
 export const FormContainer = styled.div`
   width: 50%;
+
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 
 export const Title = styled.h1`
   margin: 50px;
   margin-top: 0;
+
+  @media only screen and (max-width: 480px) {
+    margin: 20px;
+  }
 `;
 
 export const Form = styled.form`
@@ -27,6 +39,10 @@ export const Form = styled.form`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media only screen and (max-width: 480px) {
+    flex-direction: column;
+  }
 `;
 
 export const LeftForm = styled.div`
@@ -35,6 +51,11 @@ export const LeftForm = styled.div`
   flex-direction: column;
   justify-content: space-between;
   margin-right: 20px;
+
+  @media only screen and (max-width: 480px) {
+    height: 50%;
+    margin-right: 0;
+  }
 `;
 
 export const RightForm = styled.div`
@@ -42,17 +63,30 @@ export const RightForm = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+
+  @media only screen and (max-width: 480px) {
+    height: 50%;
+  }
 `;
 
 export const Input = styled.input`
   width: 200px;
   padding: 20px;
+
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+  }
 `;
 
 export const TextArea = styled.textarea`
   width: 200px;
   height: 60%;
   padding: 20px;
+
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+    margin-top: 20px;
+  }
 `;
 
 export const Button = styled.button`
@@ -64,6 +98,11 @@ export const Button = styled.button`
   border-radius: 30px;
   margin-top: 20px;
   cursor: pointer;
+
+  @media only screen and (max-width: 480px) {
+    padding: 5px;
+    font-size: 14px;
+  }
 `;
 
 export const AddressContainer = styled.div`
@@ -71,20 +110,37 @@ export const AddressContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media only screen and (max-width: 480px) {
+    width: 100%;
+    margin-top: 20px;
+  }
 `;
 
 export const AddressItem = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 50px;
+
+  @media only screen and (max-width: 480px) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const Icon = styled.img`
   width: 20px;
   margin-right: 20px;
+
+  @media only screen and (max-width: 480px) {
+    width: 15px;
+  }
 `;
 
 export const Text = styled.span`
   font-size: 20px;
   margin-right: 15px;
+
+  @media only screen and (max-width: 480px) {
+    font-size: 14px;
+  }
 `;
